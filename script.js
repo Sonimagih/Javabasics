6f6c2fb969c7aecd50f12de49f90d427c42d34c3
Sonimagih <sonimakakkoth@gmail.com>
2024-03-25 23:58:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,118 @@
+console.log("hello");
+//let x=2
+//let y=3
+//console.log(x+y)
+//console.log(x-y)
+//console.log(x*y)
+//var a=1
+//var b=2
+//console.log(a+b)
+//let a=1
+//console.log(a)
+//let b=a++
+//console.log(b)
+//let c=++a
+//console.log(c)
+//let a=5
+//let b="5"
+//if (a===b) {console.log('equal')
+    
+//} else {console.log('false')
+    
+//}
+//let person { name:"sonima",age:"10",place:"knr"};
+//console.log(person)
+//let person = {name:"sonima",
+//age:"10",
+//place:"knr"};
+//console.log(person)
+//console.log(person.age)
+//let colors=["red","blue"]
+//console.log(colors[1])
+//console.log(colors.length)
+//color[2]="grenn"
+//console.log(colors)
+//colors[2]="green"
+//console.log(colors)
+//colors.push("black")
+//console.log(colors)
+//colors.unshift("pink")
+//console.log(colors)
+//console.log(colors.unshift("white"))
+//function add(a,b)
+//{ 
+//    let c=a+b;
+//console.log(c)
+//}
+//add(6,10);
+//function sqr(a)
+//{
+  //  let d=a*a;
+    //console.log(d)
+//}
+//sqr(5);
+//function ftoc{
+  //  f=(c*(9/5))+32;
+    //crossOriginIsolated.log(f)
+//}
+//f(100)
+// ternary operator
+//let x=11;
+//console.log(x<10 ?,"failed":
+//"true")
+//for(let index=0;index<5;index++)
+ //{ console.log(index+1)}   
+//et fruits=["apple","mango","grapes","orange"] 
+//for(let index=0;index<fruits.length;index++)
+//{console.log(fruits[index])}
+//for(x of fruits){console.log(x)}
+//fruits.forEach(element=>{console.log(element)});
+//let person = {
+  //  name:"sonima",
+    //age:3,
+  //  place:"knr",
+//};
+//for (x in person)
+//{console.log(person[x]);}
+//let j = 0;
+//while (j<5)
+//{
+  //  console.log(j+1);
+    //j++
+//}
+//let fruits = ["apple","orange","banana"];
+//var k =0;
+//while(k<fruits.length)
+//{
+  // console.log(fruits[k]);
+    //k++
+//}
+//let p=1;
+//do{
+  //  console.log(p);
+    //p++
+//}while (p<=5);
+
+//FOR LOOP
+//for( let i=0;i<5;i++ )
+//{ console.log(i+1)};
+
+
+//WHILE LOOP
+let a=1;
+while (a<=5)
+{console.log(a);
+  a++;
+}
+
+
+//DO WHILE LOOP
+let b=1;
+do{console.log(b)
+b++;}while(b<=5)
+
+
+function numbers(){
+  console.log(numbers)
+}
+number(["1","2"])
